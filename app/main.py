5b72b0790e555b654d12183bfc6e7a588c14d060
Valerii <valeraivasik602@gmail.com>
2026-02-25 18:40:19 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
-import dataclasses
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 import pickle
 
+
 @dataclass
 class Specialty:
     name: str
     number: int
+
 
 @dataclass
 class Student:
@@ -19,27 +20,32 @@ class Student:
     phone_number: str
     address: str
 
+
 @dataclass
 class Group:
     specialty: Specialty
     course: int
     students: List[Student]
 
+
 def write_groups_information(groups: List[Group]) -> int:
     with open("groups.pickle", "wb") as file:
         pickle.dump(groups, file)
     return max((len(group.students) for group in groups), default=0)
+
 
 def write_students_information(students: List[Student]) -> int:
     with open("students.pickle", "wb") as file:
         pickle.dump(students, file)
     return len(students)
 
+
 def read_groups_information() -> List[str]:
     with open("groups.pickle", "rb") as file:
         groups: List[Group] = pickle.load(file)
     specialties = {group.specialty.name for group in groups}
     return list(specialties)
+
 
 def read_students_information() -> List[Student]:
     with open("students.pickle", "rb") as file:
